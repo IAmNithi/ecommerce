02a1bbe9b914ef1b1b196bfd6db75dfff8f12527
IAmNithi <kalanithi_sas@outlook.com>
2019-05-30 16:08:23 +0530

    Added Slider

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -4,7 +4,22 @@ export default class Slider extends Component {
     render() {
         return (
             <div className="slider">
-                <h1>Hello from slider</h1>
+                <div className="container">
+                    <div className="slider-main">
+                        <div className="col-12 no-padding">
+                        <div className="slider-corosal">
+                        <div className="row">
+                                    <div className="col-md-4 no-padding">
+                                        <h2>MacBook Pro</h2>
+                                    </div>
+                                    <div className="col-md-8 no-padding">
+                                        <img src="https://www.apple.com/v/mac/home/af/images/overview/hero/macbook_pro__c6uspyy0f8sy_large_2x.jpg" className="slider-img"/>
+                                    </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }

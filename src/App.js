2827a65f83b9ae85417c9a00a0a0ee1acd80bcc0
IAmNithi <kalanithi_sas@outlook.com>
2019-05-30 15:22:25 +0530

    Added Slider.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import AddBanner from './Components/AddBanner';
+import Slider from './Components/Slider';
 function App() {
   return (
     <div className="App">
     <Header />
       <AddBanner />
+      <Slider />
     </div>
   );
 }
---
+++ src/Components/Slider.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Slider extends Component {
+    render() {
+        return (
+            <div className="slider">
+                <h1>Hello from slider</h1>
+            </div>
+        )
+    }
+}

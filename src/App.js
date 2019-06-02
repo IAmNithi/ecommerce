44737ebc03f3a9f0b1f7af4c05f50cf3a6cc0779
IAmNithi <kalanithi_sas@outlook.com>
2019-06-02 18:46:59 +0530

    added top deal component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './Components/Header';
 import AddBanner from './Components/AddBanner';
 import Slider from './Components/Slider';
+import TopDeal from './Components/TopDeal';
 function App() {
   return (
     <div className="App">
     <Header />
       <AddBanner />
       <Slider />
+      <TopDeal />
     </div>
   );
 }
---
+++ src/Components/TopDeal.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TopDeal extends Component {
+    render() {
+        return (
+            <div>
+                Top Deals
+            </div>
+        )
+    }
+}

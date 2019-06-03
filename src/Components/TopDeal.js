64eae0f17e8b5fdb54f97df3356f5c6ceea96ed3
IAmNithi <kalanithi_sas@outlook.com>
2019-06-03 13:59:38 +0530

    added top deal card

--- src/Components/TopDeal.js
+++ src/Components/TopDeal.js
@@ -3,11 +3,19 @@ import React, { Component } from 'react'
 export default class TopDeal extends Component {
     render() {
         return (
-            <div className="top-deal">
+            <div className="top-deal pb-3">
                 <div className="container">
+                    <h2 className="text-center pb-3">Top Deal</h2>
                     <div className="row">
                         <div className="col-md-6">
-                            <p>Under construction</p>
+                            <div className="deal-content">
+                                <img src="https://b3h2.scene7.com/is/image/BedBathandBeyond/20190508-BBB-Homepage-C15-2-08-US?$content$&wid=440&hei=440" className="deal-img" />
+                                <div className="deal-text-content">
+                                    <h5>New Clearance Added! Deals for Every Room in Your Home</h5>
+                                    <p className="text-muted">Clearance.</p>
+                                    <p className="text-muted">While supplies last.</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>

6953ced71def1fdbf5ac92ee415ac5192d4ca8db
IAmNithi <kalanithi_sas@outlook.com>
2019-06-03 13:45:56 +0530

    added layout for top deal

--- src/Components/TopDeal.js
+++ src/Components/TopDeal.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react'
 export default class TopDeal extends Component {
     render() {
         return (
-            <div>
-                Top Deals
+            <div className="top-deal">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <p>Under construction</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }

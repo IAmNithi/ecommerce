cecfaaf99ece2778ccd88875d8e40c1a72e11118
IAmNithi <kalanithi_sas@outlook.com>
2019-06-04 15:29:39 +0530

    added feature deal card

--- src/Components/FeatureDeal.js
+++ src/Components/FeatureDeal.js
@@ -3,8 +3,20 @@ import React, { Component } from 'react'
 export default class FeatureDeal extends Component {
     render() {
         return (
-            <div>
-                Under Constaruction
+            <div className="feature-deal">
+                <div className="container">
+                    <div className="row pb-3">
+                        <div className="col-md-2 prd-card">
+                            <div className="product-card">
+                                <img src="https://b3h2.scene7.com/is/image/BedBathandBeyond/117352120205cz?$140$&wid=210&hei=210" className="card-image" />
+                                <div className="card-detail">
+                                    <h5 className="price">INR 4,219</h5>
+                                    <p className="product-desc">Anolon® Nouvelle Copper Stainless Steel French Skillet Twin Pack</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }

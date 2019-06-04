f87723ef23a290a79b6029b6361b5183ff91bb06
IAmNithi <kalanithi_sas@outlook.com>
2019-06-04 16:17:26 +0530

    product detail correction

--- src/Components/FeatureDeal.js
+++ src/Components/FeatureDeal.js
@@ -19,8 +19,8 @@ export default class FeatureDeal extends Component {
                             <div className="product-card">
                                 <img src="https://images-na.ssl-images-amazon.com/images/I/71unWeOXWlL._SL1500_.jpg" className="card-image" />
                                 <div className="card-detail">
-                                    <h5 className="price">INR 4,219</h5>
-                                    <p className="product-desc">Anolon® Nouvelle Copper Stainless Steel French Skillet Twin Pack</p>
+                                    <h5 className="price">INR 2,890.00</h5>
+                                    <p className="product-desc">Fifa 19 (PS4) by Electronic Arts</p>
                                 </div>
                             </div>
                         </div>
@@ -28,8 +28,8 @@ export default class FeatureDeal extends Component {
                             <div className="product-card">
                                 <img src="https://images-na.ssl-images-amazon.com/images/I/811czebxtnL._SL1500_.jpg" className="card-image" />
                                 <div className="card-detail">
-                                    <h5 className="price">INR 4,219</h5>
-                                    <p className="product-desc">Anolon® Nouvelle Copper Stainless Steel French Skillet Twin Pack</p>
+                                    <h5 className="price">INR 2,399.00</h5>
+                                    <p className="product-desc">God of War (PS4) by Sony</p>
                                 </div>
                             </div>
                         </div>
@@ -37,8 +37,8 @@ export default class FeatureDeal extends Component {
                             <div className="product-card">
                                 <img src="https://images-na.ssl-images-amazon.com/images/I/51IcdGlBCsL._SX323_BO1,204,203,200_.jpg" className="card-image" />
                                 <div className="card-detail">
-                                    <h5 className="price">INR 4,219</h5>
-                                    <p className="product-desc">Anolon® Nouvelle Copper Stainless Steel French Skillet Twin Pack</p>
+                                    <h5 className="price">INR 569.00</h5>
+                                    <p className="product-desc">Fire and Blood: A History of the Targaryen Kings from Aegon the Conqueror to Aegon III</p>
                                 </div>
                             </div>
                         </div>
@@ -46,8 +46,8 @@ export default class FeatureDeal extends Component {
                             <div className="product-card">
                                 <img src="https://images-na.ssl-images-amazon.com/images/I/61C%2Bz4WsbpL._SY879_.jpg" className="card-image" />
                                 <div className="card-detail">
-                                    <h5 className="price">INR 4,219</h5>
-                                    <p className="product-desc">Anolon® Nouvelle Copper Stainless Steel French Skillet Twin Pack</p>
+                                    <h5 className="price">INR 71,900.00</h5>
+                                    <p className="product-desc">Apple iPad Pro (10.5-inch, Wi-Fi, 256GB) - Space Grey</p>
                                 </div>
                             </div>
                         </div>
@@ -55,8 +55,8 @@ export default class FeatureDeal extends Component {
                             <div className="product-card">
                                 <img src="https://images-na.ssl-images-amazon.com/images/I/41FhuFnaczL.jpg" className="card-image" />
                                 <div className="card-detail">
-                                    <h5 className="price">INR 4,219</h5>
-                                    <p className="product-desc">Anolon® Nouvelle Copper Stainless Steel French Skillet Twin Pack</p>
+                                    <h5 className="price">INR 1,044.00</h5>
+                                    <p className="product-desc">Funko Pop Movies HC Spider Man with Headphones Collectible Vinyl Figure</p>
                                 </div>
                             </div>
                         </div>

cb13a226e37e87dc77bc3688edd335909532fecc
IAmNithi <kalanithi_sas@outlook.com>
2019-06-04 16:09:56 +0530

    multiple card fix

--- src/Components/FeatureDeal.js
+++ src/Components/FeatureDeal.js
@@ -6,9 +6,54 @@ export default class FeatureDeal extends Component {
             <div className="feature-deal">
                 <div className="container">
                     <div className="row pb-3">
-                        <div className="col-md-2 prd-card">
+                        <div className="col-md-2 prd-card mb-2 p-2">
                             <div className="product-card">
                                 <img src="https://b3h2.scene7.com/is/image/BedBathandBeyond/117352120205cz?$140$&wid=210&hei=210" className="card-image" />
+                                <div className="card-detail">
+                                    <h5 className="price">INR 4,219</h5>
+                                    <p className="product-desc">Anolon® Nouvelle Copper Stainless Steel French Skillet Twin Pack</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-2 prd-card mb-2 p-2">
+                            <div className="product-card">
+                                <img src="https://images-na.ssl-images-amazon.com/images/I/71unWeOXWlL._SL1500_.jpg" className="card-image" />
+                                <div className="card-detail">
+                                    <h5 className="price">INR 4,219</h5>
+                                    <p className="product-desc">Anolon® Nouvelle Copper Stainless Steel French Skillet Twin Pack</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-2 prd-card mb-2 p-2">
+                            <div className="product-card">
+                                <img src="https://images-na.ssl-images-amazon.com/images/I/811czebxtnL._SL1500_.jpg" className="card-image" />
+                                <div className="card-detail">
+                                    <h5 className="price">INR 4,219</h5>
+                                    <p className="product-desc">Anolon® Nouvelle Copper Stainless Steel French Skillet Twin Pack</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-2 prd-card mb-2 p-2">
+                            <div className="product-card">
+                                <img src="https://images-na.ssl-images-amazon.com/images/I/51IcdGlBCsL._SX323_BO1,204,203,200_.jpg" className="card-image" />
+                                <div className="card-detail">
+                                    <h5 className="price">INR 4,219</h5>
+                                    <p className="product-desc">Anolon® Nouvelle Copper Stainless Steel French Skillet Twin Pack</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-2 prd-card mb-2 p-2">
+                            <div className="product-card">
+                                <img src="https://images-na.ssl-images-amazon.com/images/I/61C%2Bz4WsbpL._SY879_.jpg" className="card-image" />
+                                <div className="card-detail">
+                                    <h5 className="price">INR 4,219</h5>
+                                    <p className="product-desc">Anolon® Nouvelle Copper Stainless Steel French Skillet Twin Pack</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-2 prd-card mb-2 p-2">
+                            <div className="product-card">
+                                <img src="https://images-na.ssl-images-amazon.com/images/I/41FhuFnaczL.jpg" className="card-image" />
                                 <div className="card-detail">
                                     <h5 className="price">INR 4,219</h5>
                                     <p className="product-desc">Anolon® Nouvelle Copper Stainless Steel French Skillet Twin Pack</p>

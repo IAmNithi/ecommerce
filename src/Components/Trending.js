e5d0b22fd510313afd23eaca5ffbae2503209f9e
IAmNithi <kalanithi_sas@outlook.com>
2019-06-06 14:44:46 +0530

    added trending base layout

--- src/Components/Trending.js
+++ src/Components/Trending.js
@@ -3,8 +3,16 @@ import React, { Component } from 'react'
 export default class Trending extends Component {
     render() {
         return (
-            <div>
-                Under Construction
+            <div className="trending">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-4"></div>
+                        <div className="col-md-4">
+                        Under Construction
+                        </div>
+                        <div className="col-md-4"></div>
+                    </div>
+                </div>
             </div>
         )
     }

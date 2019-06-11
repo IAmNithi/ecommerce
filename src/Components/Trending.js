2502c74ae29242b186a4b89e1278a57b06f20dae
IAmNithi <kalanithi_sas@outlook.com>
2019-06-11 13:29:57 +0530

    added image cards

--- src/Components/Trending.js
+++ src/Components/Trending.js
@@ -27,9 +27,36 @@ export default class Trending extends Component {
                         </div>
                         <div className="col-md-4">
                         <div className="trending-content mt-even">
-                                <img src="https://thumbor.thedailymeal.com/76KGq8CerjIeoiuRJdUdrMl78xM=/870x565/filters:focal(960x299:961x300)/https://www.thedailymeal.com/sites/default/files/story/2018/Kitchen_Tools_HERO.jpg" className="img-trending" />
+                                <img src="https://hips.hearstapps.com/rbk.h-cdn.co/assets/16/19/4000x2244/gallery-1463350734-rbk040116foodtools-001.jpg?resize=480:*" className="img-trending" />
                                 <div className="trending-text">
                                     <h5>Summer Kitchen Tools and Gadgets</h5>
+                                    <p>Shop Now</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="trending-content mt-even">
+                                <img src="http://tereleehomes.com/wp-content/uploads/2018/05/clean-bed-pillows.jpg" className="img-trending" />
+                                <div className="trending-text">
+                                    <h5>Decorative Pillows Dovia</h5>
+                                    <p>Shop Now</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                        <div className="trending-content mt-odd">
+                                <img src="https://rukminim1.flixcart.com/merch/1400/1400/images/1472728380873.jpg?q=50" className="img-trending" />
+                                <div className="trending-text">
+                                    <h5>Sports Gear</h5>
+                                    <p>Shop Now</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                        <div className="trending-content mt-even">
+                                <img src="https://i5.walmartimages.com/dfw/4ff9c6c9-5171/k2-_3b9e673e-ce18-4c17-9cb3-aeac11ec6342.v1.jpg?odnWidth=912&odnHeight=500&odnBg=ffffff" className="img-trending" />
+                                <div className="trending-text">
+                                    <h5>Outdoors and Trucking</h5>
                                     <p>Shop Now</p>
                                 </div>
                             </div>

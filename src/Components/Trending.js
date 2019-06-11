417d3ecb7f3a9024c4151149f893f39a41f64498
IAmNithi <kalanithi_sas@outlook.com>
2019-06-11 12:48:52 +0530

    added trending component

--- src/Components/Trending.js
+++ src/Components/Trending.js
@@ -6,10 +6,16 @@ export default class Trending extends Component {
             <div className="trending">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-4"></div>
                         <div className="col-md-4">
-                        Under Construction
+                            <div className="trending-content">
+                                <img src="https://a.sellpoint.net/a/NYv096vk.jpg" className="img-trending" />
+                                <div className="trending-text">
+                                    <h3>Gifts for Grill Master</h3>
+                                    <p>Shop Now</p>
+                                </div>
+                            </div>
                         </div>
+                        <div className="col-md-4"></div>
                         <div className="col-md-4"></div>
                     </div>
                 </div>

79737e25da06065431f5db8a82585f614c943fd5
IAmNithi <kalanithi_sas@outlook.com>
2019-06-13 14:08:16 +0530

    added logo to brands component

--- src/Components/Brands.js
+++ src/Components/Brands.js
@@ -3,11 +3,15 @@ import React, { Component } from 'react'
 export default class Brands extends Component {
     render() {
         return (
-            <div className="brands">
+            <div className="brands mb-3">
                 <div className="container">
                     <h3 className="text-center">Brands</h3>
                     <div className="row">
-                        
+                        <div className="col-md-2">
+                            <div className="brand-logo">
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Levis-logo-quer.svg/1200px-Levis-logo-quer.svg.png" className="brand-logo-img"/>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>

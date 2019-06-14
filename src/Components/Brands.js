c0c4d6a2735d410a6d65e075092d7ddf562c1610
IAmNithi <kalanithi_sas@outlook.com>
2019-06-14 12:49:55 +0530

    added new brand logos

--- src/Components/Brands.js
+++ src/Components/Brands.js
@@ -24,7 +24,7 @@ export default class Brands extends Component {
                         </div>
                         <div className="col-md-2">
                             <div className="brand-logo">
-                                <img src="https://www.tripadvisor.co.za/img2/eateries/Chope_3.25.2019.png" className="brand-logo-img"/>
+                                <img src="https://www.tripadvisor.co.za/img2/eateries/Chope_3.25.2019.png" className="brand-logo-img2"/>
                             </div>
                         </div>
                         <div className="col-md-2">
@@ -39,22 +39,22 @@ export default class Brands extends Component {
                         </div>
                         <div className="col-md-3">
                             <div className="brand-logo">
-                                <img src="https://pridefamilybrands.com/wp-content/uploads/2015/03/castelle-logo.png" className="brand-logo-img"/>
+                                <img src="https://www.outlet-aubonne.ch/-/media/images/b2c/switzerland/aubonne/logo-stores/ch_aub_shoppage_logo_750x500_lacoste.png?h=433&la=en&mw=650&w=650&hash=1ECE60E1017D3F315ED70DA8333EECB5BFD91FF2" className="brand-logo-img"/>
                             </div>
                         </div>
                         <div className="col-md-3">
                             <div className="brand-logo">
-                                <img src="https://www.tripadvisor.co.za/img2/eateries/Chope_3.25.2019.png" className="brand-logo-img"/>
+                                <img src="https://www.ralphlauren.asia/sites/default/files/logo.png" className="brand-logo-img2"/>
                             </div>
                         </div>
                         <div className="col-md-3">
                             <div className="brand-logo">
-                                <img src="https://www.tajhotels.com/content/dam/tajhotels/logos/taj-logos/Khazana-Logo%20gold.png" className="brand-logo-img"/>
+                                <img src="https://i1.adis.ws/i/liberty/liberty-london-logo-gold.png" className="brand-logo-img"/>
                             </div>
                         </div>
                         <div className="col-md-3">
                             <div className="brand-logo">
-                                <img src="https://logos-download.com/wp-content/uploads/2016/11/GAS_Jeans_logo_wordmark.png" className="brand-logo-img"/>
+                                <img src="https://cdn10.bigcommerce.com/s-qzzxzg/templates/__custom/images/MITCH-n-NESS.png?t=1554974146" className="brand-logo-img"/>
                             </div>
                         </div>
                     </div>

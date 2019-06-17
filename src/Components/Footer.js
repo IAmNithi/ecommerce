e51024ee60d801f91df01abddaf20170296441a6
IAmNithi <kalanithi_sas@outlook.com>
2019-06-17 15:26:53 +0530

    package json renameed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
     render() {
         return (
-            <div>
-                Footer
+            <div className="footer">
+                <div className="container">
+                this is footer
+                </div>
             </div>
         )
     }

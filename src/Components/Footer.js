eeb98f871c61c6d712fd30f56226597ef8941792
IAmNithi <kalanithi_sas@outlook.com>
2019-06-20 14:57:24 +0530

    added footer content

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,18 @@ export default class Footer extends Component {
             <div className="footer">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-4"></div>
+                        <div className="col-md-4">
+                            <div className="footer-cnt">
+                                <h4>GET IN TOUCH</h4>
+                                <ul>
+                                    <li>1800-00-000</li>
+                                    <li>Email Us</li>
+                                    <li>Facebook</li>
+                                    <li>FAQ</li>
+                                    <li>Update your Email Preferance</li>
+                                </ul>
+                            </div>
+                        </div>
                         <div className="col-md-4"></div>
                         <div className="col-md-4"></div>
                     </div>

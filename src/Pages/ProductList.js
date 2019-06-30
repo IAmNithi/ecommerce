24b11ae0729a22055c9626e1fea6ec88c56e1aba
IAmNithi <kalanithi_sas@outlook.com>
2019-06-30 21:46:47 +0530

    added header to product list app

--- src/Pages/ProductList.js
+++ src/Pages/ProductList.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
+import Header from '../Components/Header';
 export default class ProductList extends Component {
     render() {
         return (
             <div>
-                
+                <Header />
             </div>
         )
     }

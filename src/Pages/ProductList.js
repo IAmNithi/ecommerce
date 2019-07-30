36a322c520543ffc321a1cbc49937bc68c14272e
IAmNithi <kalanithi_sas@outlook.com>
2019-07-30 17:39:53 +0530

    added footer

--- src/Pages/ProductList.js
+++ src/Pages/ProductList.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import Header from '../Components/Header';
+import Footer from '../Components/Footer';
 export default class ProductList extends Component {
     render() {
         return (
             <div>
                 <Header />
+                <Footer />
             </div>
         )
     }
